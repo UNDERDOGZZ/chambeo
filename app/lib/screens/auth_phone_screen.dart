b8d7f70d1c9cb1101c82c1c8db2fd2404769563c
Ricardo Alfredo Guevara Aparicio <43429167+UNDERDOGZZ@users.noreply.github.com>
2025-12-29 02:58:00 -0500

    task-07: add router and global state

---
+++ app/lib/screens/auth_phone_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AuthPhoneScreen extends StatelessWidget {
+  const AuthPhoneScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Auth Phone'),
+      ),
+    );
+  }
+}
